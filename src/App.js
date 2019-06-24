7e701be3bf1eb2b01c9b2de5d2f3edeab133a0d2
LiveSlowSkateFast <jp@jprowan.com>
2019-06-24 12:16:20 -0700

    protected profile page with private route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import NavBar from "./components/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
 
+// NEW - import the PrivateRoute component
+import PrivateRoute from "./components/PrivateRoute";
+import ExternalApi from "./components/ExternalApi";
+import { useAuth0 } from "./react-auth0-wrapper";
+
 function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
   return (
     <div className="App">
       <BrowserRouter>
@@ -12,7 +22,9 @@ function App() {
         </header>
         <Switch>
           <Route path="/" exact />
-          <Route path="/profile" component={Profile} />
+
+          {/* NEW - Modify the /profile route to use PrivateRoute instead of Route */}
+          <PrivateRoute path="/profile" component={Profile} />
         </Switch>
       </BrowserRouter>
     </div>
--- src/index.js
+++ src/index.js
@@ -5,8 +5,23 @@ import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-wrapper";
 import config from "./auth_config.json";
 
+
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
 ReactDOM.render(
-  <Auth0Provider domain={config.domain} client_id={config.clientId}>
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    onRedirectCallback={onRedirectCallback}
+  >
     <App />
   </Auth0Provider>,
   document.getElementById("root")
