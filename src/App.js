1a21ddecbbae388f3088b2277cf2e0678d01f07c
LiveSlowSkateFast <jp@jprowan.com>
2019-06-24 12:11:29 -0700

    Added profile page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import NavBar from "./components/Navbar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <NavBar />
-      </header>
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
