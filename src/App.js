31c9d857c07ff60c81d305581542d96b534daa58
LiveSlowSkateFast <jp@jprowan.com>
2019-07-02 15:43:10 -0700

    Added loading component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Profile from "./components/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import ExternalApi from "./components/ExternalApi";
+import Loading from "./components/Loading"
 import { useAuth0 } from "./react-auth0-wrapper";
 
 function App() {
   const { loading } = useAuth0();
 
   if (loading) {
-    return <div>Loading...</div>
+    return <Loading />;
   }
   return (
     <div className="App">
