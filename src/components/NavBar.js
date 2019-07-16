48222676118a9527b398de1533da988f4792a4e8
LiveSlowSkateFast <jp@jprowan.com>
2019-07-15 20:19:41 -0700

    simplified nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,29 +3,21 @@ import { NavLink as RouterNavLink } from "react-router-dom";
 
 import {
   Row,
-  Collapse,
   Container,
   Navbar,
-  NavbarToggler,
   NavbarBrand,
   Nav,
-  NavItem,
 } from "reactstrap";
 
 import { useAuth0 } from "../react-auth0-wrapper";
 import { Logo, Button } from "@auth0/cosmos";
 
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
   const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
-
-  const toggle = () => {
-    setIsOpen({ isOpen: !isOpen });
-  };
 
   return (
     <div className="nav-container">
-      <Navbar color="light" light expand="md">
+      <Navbar color="light" light >
         <Container>
           <NavbarBrand tag={RouterNavLink} to="/" >
             <Row>
@@ -33,24 +25,17 @@ const NavBar = () => {
               React SPA Demo
             </Row>
           </NavbarBrand>
-          <NavbarToggler onClick={toggle} />
-          <Collapse isOpen={isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-            </Nav>
-            <Nav className="d-none d-md-block" navbar>
-              <NavItem>
-                <Button
-                  id={isAuthenticated ? "qsLogoutBtn" : "qsLoginBtn"}
-                  color="primary"
-                  className="btn-margin"
-                  onClick={isAuthenticated ?
-                    () => logout({}) :
-                    () => loginWithRedirect({ redirect_uri: window.location.origin })}>
-                  {isAuthenticated ? "Log out" : "Log in"}
-                </Button>
-              </NavItem>
-            </Nav>
-          </Collapse>
+          <Nav className="ml-auto" navbar>
+            <Button
+              id={isAuthenticated ? "qsLogoutBtn" : "qsLoginBtn"}
+              color="primary"
+              className="btn-margin"
+              onClick={isAuthenticated ?
+                () => logout({}) :
+                () => loginWithRedirect({ redirect_uri: window.location.origin })}>
+              {isAuthenticated ? "Log out" : "Log in"}
+            </Button>
+          </Nav>
         </Container>
       </Navbar>
     </div>
