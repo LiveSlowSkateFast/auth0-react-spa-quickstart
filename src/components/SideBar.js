b53b6ced92ee060f9a8240609f15de84c77d4b5d
LiveSlowSkateFast <jp@jprowan.com>
2019-07-18 12:32:37 -0700

    Navigation tweeks

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,49 +7,44 @@ import { useAuth0 } from "../react-auth0-wrapper";
 const SideBar = () => {
   const { isAuthenticated } = useAuth0();
   return (
-      <Col lg='2' style={{
-        borderRightColor: "black",
-        borderRightWidth: "1px",
-        borderRightStyle: "solid",
-      }}>
-        <Nav className="ml-auto" navbar>
-          {isAuthenticated && (
-            <>
-              <NavItem>
-                <NavLink
-                  tag={RouterNavLink}
-                  to="/profile"
-                  exact
-                  activeClassName="router-link-exact-active"
-                >
-                  Profile
-          </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  tag={RouterNavLink}
-                  to="/enrollments"
-                  exact
-                  activeClassName="router-link-exact-active"
-                >
-                  Enrollments
-          </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  tag={RouterNavLink}
-                  to="/external-api"
-                  exact
-                  activeClassName="router-link-exact-active"
-                >
-                  External API
-          </NavLink>
-              </NavItem>
-            </>
-          )}
-        </Nav>
-      </Col>
-        );
-      }
+    <Col lg='2' style={{
+      borderRightColor: "black",
+      borderRightWidth: "1px",
+      borderRightStyle: "solid",
+    }}>
+      <Nav className="ml-auto" navbar>
+        {isAuthenticated && (
+          <>
+            <NavItem>
+              <NavLink tag={RouterNavLink} to="/profile" exact activeClassName="router-link-exact-active">
+                Edit Profile
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RouterNavLink} to="/enrollments" exact activeClassName="router-link-exact-active">
+                MFA Enrollments
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RouterNavLink} to="/" exact activeClassName="router-link-exact-active">
+                Update Password
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RouterNavLink} to="/" exact activeClassName="router-link-exact-active">
+                Identities
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink tag={RouterNavLink} to="/" exact activeClassName="router-link-exact-active">
+                Consents
+              </NavLink>
+            </NavItem>
+          </>
+        )}
+      </Nav>
+    </Col>
+  );
+}
 
-      export default SideBar;
+export default SideBar;
